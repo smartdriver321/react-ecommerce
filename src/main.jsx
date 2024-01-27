7598d54bf2a1d04fd824951dd7f8309fffe3f00d
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-27 14:36:05 +0700

    Create About and Contact page

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,9 @@ import Shop from './pages/Shop/Shop.jsx'
 import SingleProduct from './pages/Shop/SingleProduct.jsx'
 import CartPage from './pages/Shop/CartPage.jsx'
 import SingleBlog from './pages/Blog/SingleBlog.jsx'
+import AboutUs from './pages/Home/AboutUs.jsx'
+import About from './pages/AboutPage/About.jsx'
+import Contact from './pages/ContactPage/Contact.jsx'
 
 const router = createBrowserRouter([
   {
@@ -49,6 +52,14 @@ const router = createBrowserRouter([
       {
         path: '/cart-page',
         element: <CartPage />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
       },
     ],
   },
